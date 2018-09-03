0dbbcfe5e23915abbf1637dd80edd6a3355ee165
Sehyun Av Kim <sk5642@nyu.edu>
2018-09-02 23:53:51 -0400

    first commit

---
+++ Assets/Scripts/RayTracer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayTracer : MonoBehaviour {
+    public ComputeShader mRayTracer;
+
+    private const int w = 1280, h = 720;
+    private const float ratio = (float)w / (float)h;
+    private const int numThreads = 8;
+    private const int numGroupX = w / numThreads;
+    private const int numGroupY = h / numThreads;
+    private int kernel;
+
+    private RenderTexture _renderOut;
+    public RenderTexture renderOut
+    {
+        get { return _renderOut; }
+    }
+
+	void Start ()
+    {
+        InitResources();
+	}
+	
+	void Update ()
+    {
+        DispatchComputeShader();
+	}
+
+    void DispatchComputeShader()
+    {
+        mRayTracer.SetTexture(kernel, "RenderOut", _renderOut);
+
+        mRayTracer.Dispatch(kernel, numGroupX, numGroupY, 1);
+    }
+
+    void InitResources()
+    {
+        // render texture for compute shader output
+        _renderOut = new RenderTexture(w, h, 0);
+        _renderOut.format = RenderTextureFormat.ARGB32;
+        _renderOut.filterMode = FilterMode.Bilinear;
+        _renderOut.wrapMode = TextureWrapMode.Clamp;
+        _renderOut.enableRandomWrite = true;
+        _renderOut.Create();
+
+        // setup compute shader 
+        kernel = mRayTracer.FindKernel("RayTrace");
+
+        mRayTracer.SetFloat("uWidth", (float)w);
+        mRayTracer.SetFloat("uHeight", (float)h);
+        mRayTracer.SetFloat("uRatio", ratio);
+    }
+
+    void DestroyResources()
+    {
+        // destroy render texture
+        if(_renderOut != null)
+        {
+            _renderOut.Release();
+            _renderOut = null;
+            Destroy(_renderOut);
+        }
+
+    }
+}
---
+++ Assets/Scripts/RenderImage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderImage : MonoBehaviour {
+    public RayTracer mRayTracer;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(mRayTracer.renderOut, destination);
+    }
+}
